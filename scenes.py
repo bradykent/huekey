b6c05d51d061cdfc1803cc6f576d92cf2d7aee8d
bradykent <brady@bradykent.org>
2013-07-21 12:46:12 -0600

    added better documentation and example scripts

--- scenes.py
+++ scenes.py
@@ -18,7 +18,14 @@ def all_on():
 def all_off():
 	b.set_light(lights,'on',False)
 
-# Example to set a single light with easily visible settings
+# Will turn light 1 on or off
+def example():
+	if b.get_light(1,'on') is False:
+		b.set_light(1, 'on', True)
+	else:
+		b.set_light(1, 'on', False)
+
+# Example to set a single light with easily visible settings. The
 def nightlight():
 	if b.get_light(1,'on') is False:
 		lights[1].on = True
@@ -29,11 +36,19 @@ def nightlight():
 		lights[1].on = False
 
 # Example to set all lights to the same setting.
+# evening is defined by running a b.get_light(1)['state'] command in terminal while the 'evening light' preset is running on light 1, then copying the result and pasting it below.
 def eveninglight():
 	evening = {u'on': True, u'hue': 46256, u'colormode': u'ct', u'effect': u'none', u'alert': u'none', u'xy': [0.179, 0.0626], u'reachable': True, u'bri': 127, u'sat': 251, u'ct': 244}
 	b.set_light(lights, evening)
 
-# Creating a scene
+# Creating a scene where lights 2 and 3 have the same setting and light 1 is unaffected
 def deepsea():
 	deepsea = {u'on': True, u'hue': 65496, u'colormode': u'xy', u'effect': u'none', u'alert': u'none', u'xy': [0.6727, 0.3216], u'reachable': True, u'bri': 183, u'sat': 253, u'ct': 500}
 	b.set_light([2,3], deepsea)
+
+# Creating a scene where all three lights are involved and have different settings
+# Each light's settings were discovered by activating the "Taj" preset in the Hue app then running a b.get_light(LIGHTNUMBER)['state'] command in terminal for all three lights, then pasting the results below
+def taj():
+	b.set_light(1, {u'on': True, u'hue': 58985, u'colormode': u'xy', u'effect': u'none', u'alert': u'none', u'xy': [0.3889, 0.3652], u'reachable': True, u'bri': 166, u'sat': 19, u'ct': 262})
+	b.set_light(2, {u'on': True, u'hue': 64206, u'colormode': u'xy', u'effect': u'none', u'alert': u'none', u'xy': [0.3936, 0.3729], u'reachable': True, u'bri': 181, u'sat': 13, u'ct': 267})
+	b.set_light(3, {u'on': True, u'hue': 57049, u'colormode': u'xy', u'effect': u'none', u'alert': u'none', u'xy': [0.3859, 0.3601], u'reachable': True, u'bri': 196, u'sat': 23, u'ct': 256})
